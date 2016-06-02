9eeb979114744ba3b0aab2ac8ec41d0aac2cba75
Carter Templeton <ctempleton89@gmail.com>
2016-06-02 15:06:44 -0400

    Update Java files, add Java class files

--- PardotTest/src/secretKeeper.java
+++ PardotTest/src/secretKeeper.java
@@ -5,56 +5,54 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class secretKeeper {
-    public secretKeeper(){}
 
-    public int secret(int n){
-        /*magic. this is the most basic definition of a function that takes in an int and returns an int.
+    public int secret(int m){
+        /*magic. This is a stub that just takes in an int and returns it.
             Just replace it with your secret code. Make sure it returns an int!*/
-        return n;
+        return m;
     }
 
     public boolean isSecretAdditive(int n){
         List<Integer> primes = primes(n);
 
-        boolean additive = true;
-
-        for(int foo = 0; foo< primes.size()-1; foo++){
+        for(int foo = 0; foo< primes.size(); foo++){
             int x = primes.get(foo);
-            for (int bar = foo+1; bar<primes.size(); bar++){
+            for (int bar = foo; bar<primes.size(); bar++){
                 int y = primes.get(bar);
                 int foobar = x+y;
                 if(secret(x)+ secret(y) != secret(foobar)) return false;
             }
         }
-        return additive;
+        return true;
     }
 
     public List<Integer> primes(int max){
         List<Integer> primes = new ArrayList<Integer>();
 
-        if (max < 2) return primes; //0 and 1 are not primes
         primes.add(2); //2 is the first prime; we're accepting this as a given
-        if (max == 2 ){
-            return primes;
-        }
+        if (max == 2 ) return primes;
 
         boolean[] generator = new boolean[max+1];
-        for(int i = 3; i < max; i++) generator[i] = true;
+        for(int i = 3; i <= max; i++) generator[i] = true;
         //int loops = 0; //tracker for debugging
 
         for(int i = 3; i<= max; i+=2){
             if (generator[i]){
+                //loops++;
                 primes.add(i);
-                for (int j = i*i; j<=max; j+=(i*2)) {
-                    //loops++; //for debugging
-                    generator[j] = false;}
+                if (i< Math.sqrt(max)) {
+                    for (int j = i * i; j <= max; j += (i * 2)) {
+                        //loops++; //for debugging
+                        generator[j] = false;
+                    }
+                }
             }
         } /*this is a modified version of the Sieve of Eratosthenes algorithm for generating primes.
                 it's been optimized to skip all even numbers, b/c we know 2 is a prime and the rest have 2 as a factor.
-            the result is 70%+ fewer loops*/
+            the result is 70%+ fewer loop iterations*/
 
-        //you can uncomment these if you're curious. remember to uncomment line 43 if you uncomment line 57
-        //System.out.println("That was " + loops + " loops");
+        //you can uncomment these if you're curious. uncomment 37, 41, 45, and 55 to track all the loop iterations
+        //System.out.println("That was " + loops + " loop iterations");
         //System.out.println("There are " + primes.size() + " primes below " + max);
         return primes;
     }
